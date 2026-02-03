c56495b2d0fa99e872072e27417512e0a4c40111
shlok728 <shloksingh612@gmail.com>
2026-02-03 22:22:57 +0530

    Add files via upload

--- backend/models/Report.js
+++ backend/models/Report.js
@@ -1,1 +1,14 @@
+const mongoose = require("mongoose")
 
+
+const reportSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+  image: { type: String },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  status: { type: String, default: "Pending" },
+  createdAt: { type: Date, default: Date.now },
+  location: {lat: Number,lng: Number},
+
+})
+
+module.exports = mongoose.model("Report", reportSchema)
