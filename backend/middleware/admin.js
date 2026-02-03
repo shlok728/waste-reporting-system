d24b829cf29e2592acebeb46c5589a2a6d43d52f
shlok728 <shloksingh612@gmail.com>
2026-02-03 22:20:52 +0530

    Add files via upload

--- backend/middleware/admin.js
+++ backend/middleware/admin.js
@@ -1,1 +1,7 @@
-
+module.exports = function (req, res, next) {
+  console.log("ADMIN CHECK ROLE:", req.user.role)
+  if (req.user.role !== "admin") {
+    return res.status(403).json({ msg: "Admin access denied" })
+  }
+  next()
+}
